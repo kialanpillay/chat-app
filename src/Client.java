e33f680d94d82c89fde56650521521ed5a3e9587
Kialan Pillay <kialanpillay@gmail.com>
2020-02-22 19:24:46 +0200

    Update to use CLI

--- src/Client.java
+++ src/Client.java
@@ -2,22 +2,21 @@ package src;
 
 import java.io.*;
 import java.net.*;
-import java.util.Scanner;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 public class Client {
 
     private static Socket socket;
-    private static String fileName;
-    private static Scanner in;
+    private static int port;
+    private static String operation;
     private static Protocol protocol;
     private static PrintStream os;
 
     public static void main(String[] args) throws IOException {
+        port = Integer.parseInt(args[1]);
         try {
-            socket = new Socket("localhost", 8080);
-            in = new Scanner(System.in);
+            //socket = new Socket("localhost", port);
+            InetAddress address = InetAddress.getByName(args[0]);
+            socket = new Socket(address, port);
             protocol = new Protocol(socket);
         } catch (Exception e) {
             System.err.println("Cannot connect to the server, try again later.");
@@ -25,43 +24,27 @@ public class Client {
         }
 
         os = new PrintStream(socket.getOutputStream());
-        String option = "";
 
-        while(!option.equals("Q")){
-            System.out.println("FileShare Application");
-            System.out.println("=====================");
-            System.out.println("MENU");
-            System.out.println("1) Upload File");
-            System.out.println("2) Download File");
-            System.out.println("3) List Stored Files");
-            System.out.println("Q) Quit\n");
-            //src/test.txt
 
-            option = in.nextLine();
-            if(!option.equals("Q")){
+        System.out.println("FileShare Application");
+        System.out.println("=====================");
 
-                switch (option) {
-                    case "1":
+        if(!operation.equals("-l")){
+            String fileName = args[3];
+            switch (operation) {
+                case "-u":
                         os.println("1");
-                        System.out.println("Enter file name: ");
-                        fileName = in.nextLine();
                         protocol.sendFile(fileName);
                         break;
-                    case "2":
+                case "-d":
                         os.println("2");
-                        System.out.println("Enter file name: ");
-                        fileName = in.nextLine();
                         protocol.receiveFile(fileName);
                         break;
-                    case "3":
-                        os.println("3");
-                        protocol.listFiles();
-                        break;
-                    default:
-                        System.out.println("Invalid Option");
-
-                }
             }
+        }
+        else{
+            os.println("3");
+            protocol.listFiles();
         }
         socket.close();
     }
