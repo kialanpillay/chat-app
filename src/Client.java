ff0dd6badb8e119cb32dc84bba561eaef02cf046
Kialan Pillay <kialanpillay@gmail.com>
2020-02-22 22:03:05 +0200

    Error Handling

--- src/Client.java
+++ src/Client.java
@@ -12,44 +12,50 @@ public class Client {
     private static PrintStream os;
 
     public static void main(String[] args) throws IOException {
-        port = Integer.parseInt(args[1]);
-        operation = args[2];
-        try {
-            //socket = new Socket("localhost", port);
-            InetAddress address = InetAddress.getByName(args[0]);
-            socket = new Socket(address, port);
-            protocol = new Protocol(socket);
-        } catch (Exception e) {
-            System.err.println("Cannot connect to the server, try again later.");
-            System.exit(1);
-        }
 
-        os = new PrintStream(socket.getOutputStream());
-
-
-        System.out.println("FileShare Application");
-        System.out.println("=====================");
-
-        if(!operation.equals("-l")){
-            String fileName = args[3];
-            switch (operation) {
-                case "-u":
-                //Request r = new Request()
-                        os.println("1"); //os.printlin(r.getHeader())
-                        protocol.sendFile(new File(fileName));
-                        break;
-                case "-d":
-                        os.println("2");
-                        os.println(fileName);
-                        protocol.receiveFile(fileName);
-                        break;
-            }
+        if(args.length < 3){
+            System.out.println("Incorrect number of arguments!");
         }
         else{
-            os.println("3");
-            protocol.listFiles();
-        }
-        socket.close();
+            port = Integer.parseInt(args[1]);
+            operation = args[2];
+            try {
+                //socket = new Socket("localhost", port);
+                InetAddress address = InetAddress.getByName(args[0]);
+                socket = new Socket(address, port);
+                protocol = new Protocol(socket);
+            } catch (Exception e) {
+                System.err.println("Cannot connect to the server, try again later.");
+                System.exit(1);
+            }
+
+            os = new PrintStream(socket.getOutputStream());
+
+
+            System.out.println("FileShare Application");
+            System.out.println("=====================");
+
+            if(!operation.equals("-l")){
+                String fileName = args[3];
+                switch (operation) {
+                    case "-u":
+                    //Request r = new Request()
+                            os.println("1"); //os.printlin(r.getHeader())
+                            protocol.sendFile(new File(fileName));
+                            break;
+                    case "-d":
+                            os.println("2");
+                            os.println(fileName);
+                            protocol.receiveFile(fileName);
+                            break;
+                }
+            }
+            else{
+                os.println("3");
+                protocol.listFiles();
+            }
+            socket.close();
+    }
     }
 
     
