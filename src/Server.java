bfa775124e3b26c75ff5023f25b9be34fd6b42d5
insaafdhansay <insaafdhansay@gmail.com>
2020-02-26 08:21:07 +0200

    write textfile

--- src/Server.java
+++ src/Server.java
@@ -9,13 +9,33 @@ public class Server{
     private static int port;
     private static Socket clientSocket = null;
     private static ServerSocket serverSocket = null;
-    public static ArrayList<String> fileNames = new ArrayList<>();
-    public static ArrayList<String> permissions = new ArrayList<>();
-    public static ArrayList<String> keys = new ArrayList<>();
+    static ArrayList<String> fileNames = new ArrayList<String>();
+    static ArrayList<String> permissions = new ArrayList<String>();
+    static ArrayList<String> keys = new ArrayList<String>();
     public static void main (String [] args ) throws IOException {
+        
+        
+//reading in 
+
+    Scanner f  = new Scanner(new File("Files.txt"));
+    while(f.hasNext()){
+    
+
+
+
+
+
+    }
+
+
+
+
+
+
         if(args.length < 1){
             System.out.println("Incorrect number of arguments!");
             System.exit(1);
+            
         }
         else{
         port = Integer.parseInt(args[0]);
@@ -25,6 +45,7 @@ public class Server{
 
             File file = new File("server");
             file.mkdir();
+
         } catch (Exception e) {
             System.err.println("Port already in use.");
             System.exit(1);
@@ -45,10 +66,24 @@ public class Server{
             }
         }
         //TODO
+        try{
+            FileWriter writer = new FileWriter("Files.txt",true);
+            for(int i=0;i<fileNames.size();i++){
+                writer.write(fileNames.get(i)+",");
+                writer.write(permissions.get(i)+",");
+                writer.write(keys.get(i));
+
+
+            }
+        }
+        catch(IOException e){
+            e.printStackTrace();
+        }
+
+        }
         serverSocket.close();
         System.out.println("FileShare Server stopped");
         System.exit(0);
     }
 
     }
-}
